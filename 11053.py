4aeb3d1ba1842f3af280c330cc3fceb7cca1d917
gunho0809 <gunho0809@naver.com>
2022-07-16 05:17:11 +0900

    11053

---
+++ 11053.py
@@ -0,0 +1,18 @@
+n = input() #수열의 크기
+s = input() # 수열
+
+n = int(n)
+s = list(map(int, s.split(' ')))
+table = [0] * n
+
+for i in range(n):
+    max_b = 0
+    for j in range(i):
+        if s[j] < s[i]:
+            max_b = max(max_b, table[j])
+    table[i] = max_b + 1
+
+print(max(table))
+
+
+
